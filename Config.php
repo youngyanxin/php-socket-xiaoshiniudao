04addf5dbd0a054cbdf6bd526c7d7066112ba008
yyx <youngyanxin@163.com>
2020-08-18 18:39:48 +0800

    新增配置文件

---
+++ Config.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: h
+ * Date: 2020/8/18
+ * Time: 18:33
+ */
+
+namespace socket;
+
+
+final class Config
+{
+    const
+    //IP
+    HOST_IP = '127.0.0.1',
+    //PORT
+    HOST_PORT = '8999';
+
+}
